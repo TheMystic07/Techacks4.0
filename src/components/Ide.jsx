60621fe9f18bf00bce13000d3fd34df148dff730
Mystic <gushabad90@gmail.com>
2024-01-20 03:14:07 +0530

    made board

---
+++ src/components/Ide.jsx
@@ -0,0 +1,13 @@
+'use client'
+const Ide = () => {
+  return (
+    <div>
+      <div data-pym-src="https://www.jdoodle.com/plugin" data-language="java"  data-version-index="4" data-libs="mavenlib1, mavenlib2">
+            
+        </div>
+        <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js" type="text/javascript"> </script>
+    </div>
+  )
+}
+
+export default Ide
