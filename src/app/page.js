938f3f7bd663a52a6bdbde9daaeec8f0cfe7c9d1
Satyansh <mittalsatyansh@gmail.com>
2024-01-20 10:54:30 +0530

    Ide half done

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,14 @@ import Ide from "@/components/Ide";
 
 export default function Home() {
   return (
-    // <Board />
-    <Ide suppressHydrationWarning />
+
+    <body>
+      {/* <Board /> */}
+      <div data-pym-src="https://www.jdoodle.com/plugin" data-language="java" data-version-index="4" data-libs="mavenlib1, mavenlib2">
+        Optional default code goes here
+      </div>
+      <script src="https://www.jdoodle.com/assets/jdoodle-pym.min.js" type="text/javascript"> </script>
+    </body>
+    // <Ide suppressHydrationWarning />
   );
 }
